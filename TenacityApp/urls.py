44d5ca6e370eaa75caeb5bfbba216ba49a90d18c
Akinro Akintunde <aktakinro@gmail.com>
2022-03-09 08:36:57 -0800

    Added models for comment and posts to social app

--- TenacityApp/urls.py
+++ TenacityApp/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions # new
+from drf_yasg.views import get_schema_view # new
+from drf_yasg import openapi # new
+
+# this is to change the admin dashboard header
+admin.site.site_header = 'Store Front Admin'
+admin.site.index_title = 'Admin'
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Ecommerce API",
+        default_version="v1",
+        description="Api documentation for our first api.",
+        terms_of_service="",
+        contact=openapi.Contact(email="aktakinro@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('account.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- social/models.py
+++ social/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    username = models.CharField(max_length=12)
+    caption = models.CharField(max_length=255)
+    # post_image = models.ImageField()
+    # profile_picture = models.image
+    likes = models.IntegerField()
+    time_posted = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title 
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
